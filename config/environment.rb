7469695bbcbf61bf98599c7d823965e195e828df
Cole Dac <collindavis@berkeley.edu>
2025-02-13 16:35:06 -0800

    3 deploymen56

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
+
 @secret = "rrh7PtBApWPj57mBGyS8zw=="
+puts @secret.bytesize
+puts "HEYYYYY MACARENA"
 # Initialize the Rails application.
 Rails.application.initialize!
