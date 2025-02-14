fb746dd2e8149ea50d38925d52d27a6507bb3d5f
Cole Dac <collindavis@berkeley.edu>
2025-02-13 16:23:14 -0800

    3 deploymen54

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-@secret = b'\xae\xb8{>\xd0@\xa5c\xe3\xe7\xb9\x81\x1b$\xbc\xcf'
+@secret = "\xae\xb8\x7b\x3e\xd0\x40\xa5\x63\xe3\xe7\xb9\x81\x1b\x24\xbc\xcf"
 # Initialize the Rails application.
 Rails.application.initialize!
