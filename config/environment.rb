a0318750bb69879da2769807c87a9da7d153fb8e
garrettwiththepegleg <poipre@CollinsPet.>
2025-02-16 01:53:47 -0800

    heyhey

--- config/environment.rb
+++ config/environment.rb
@@ -1,31 +1,5 @@
+# Load the Rails application.
 require_relative "application"
-require 'base64'
-require 'openssl'
 
-# Fetch the base64-encoded key from the environment variable
-
-
-# Check if the environment variable is set
-require 'base64'
-require 'openssl'
-
-# Base64-encoded key
-base64_key = "rrh7PtBApWPj57blablard=="
-
-# Decode the base64 string to get the original 16-byte key
-@secret = Base64.decode64(base64_key)
-
-puts @secret.bytesize
-
-
-
-# Initialize the cipher with the key
-cipher = OpenSSL::Cipher.new('aes-128-cbc')
-cipher.encrypt
-cipher.key = @secret
-
-
-puts "Key length: #{@secret.bytesize}"
-puts "Key: #{@secret.inspect}"
 # Initialize the Rails application.
 Rails.application.initialize!
